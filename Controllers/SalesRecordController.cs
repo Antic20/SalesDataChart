269d3fcac7fee1b2261cb42ff41405b4c7b08453
Antic20 <aleksandarantic.inf@gmail.com>
2024-01-25 15:17:16 +0100

    commit
    Date: 25-01-2024

---
+++ Controllers/SalesRecordController.cs
@@ -0,0 +1,44 @@
+﻿using SalesDataChart.DataLayer;
+using SalesDataChart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesDataChart.Controllers
+{
+    public class SalesRecordController : Controller
+    {
+        private readonly DbContextSales _context;
+
+        public SalesRecordController(DbContextSales context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    public IActionResult ShowSalesData()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public List<object> GetSalesDate() 
+        {
+            List<object> data = new List<object>();
+
+            List<string> labels=_context.SalesData.Select(p=>p.Monthname).ToList();
+            data.Add(labels);
+
+            data.Add(labels);
+
+            List<int> SalesNumber = _context.SalesData.Select(p => p.TotalSales).ToList();
+            data.Add(SalesNumber);
+
+            return data;
+        }
+
+
+
+    }
+}
---
+++ DataLayer/DbContextSales.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesDataChart.Models;
+
+namespace SalesDataChart.DataLayer
+{
+    public class DbContextSales :DbContext
+    {
+        public DbContextSales(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<SalesEntity> SalesData { get; set; }
+    }
+}
